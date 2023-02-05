522ab19edfaa3c11ed5195ba2b949d33abbc54b4
andrimation <andrimation@gmail.com>
2023-02-05 18:43:09 +0100

    test dla home_page ktory przeszedl

--- TDD/urls.py
+++ TDD/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import lists.views
 
 urlpatterns = [
+    path('',lists.views.home_page),
     path('admin/', admin.site.urls),
 ]
--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,19 @@
+from django.urls import resolve
+from lists.views import home_page
 from django.test import TestCase
 # django.test -> to rozszeżona wersja unittest dla django !
 
 # Create your tests here.
 
-class SmokeTest(TestCase):
+# class SmokeTest(TestCase):
+#
+#     def test_bad(self):
+#         self.assertEqual(1+1,3)
 
-    def test_bad(self):
-        self.assertEqual(1+1,3)
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        # resolve służy dopasowaniu adresu url do konkretnej funkcji widoku. - zwraca
+        # funkcję która ma zostać wykonana po otrzymaniu określonego adresu url
+        found = resolve('/')
+        self.assertEqual(found.func,home_page)
